5205d7bb3d618139122192fc23385bf6d94d555c
Andrew Budziszek <andrew@budziszek.com>
2016-09-19 21:59:28 -0500

    Initial Commit

---
+++ ProjectEulerSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEulerSwift
+//
+//  Created by Andrew Budziszek on 9/19/16.
+//  Copyright © 2016 Andrew Budziszek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
